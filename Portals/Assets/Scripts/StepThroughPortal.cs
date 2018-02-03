2f40115fece31fdab09dbda1724e5b431da971d1
Jbakercoe <jbakercoe@gmail.com>
2018-02-03 14:20:26 -0500

    Add Unity Project

---
+++ Portals/Assets/Scripts/StepThroughPortal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StepThroughPortal : MonoBehaviour {
+
+    [SerializeField] GameObject otherPortal;
+    [SerializeField] Transform player;
+
+    private new Camera camera;
+
+	// Use this for initialization
+	void Start () {
+        camera = GetComponentInChildren<Camera>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.position = otherPortal.transform.position + (otherPortal.transform.forward * 2);
+            other.transform.rotation = otherPortal.transform.rotation;
+        }
+    }
+
+}
---
+++ Portals/Assets/Scripts/ThrowPortal.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class ThrowPortal : MonoBehaviour {
+    
+    [SerializeField] GameObject leftPortal;
+    [SerializeField] GameObject rightPortal;
+
+    GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        Assert.IsNotNull(mainCamera);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Left click");
+            throwPortal(leftPortal);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            print("Right click");
+            throwPortal(rightPortal);
+        }
+    }
+
+    private void throwPortal(GameObject portal) {
+        
+        int x = Screen.width / 2;
+        int y = Screen.height / 2;
+
+        Ray ray = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector3(x, y));
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            Quaternion hitObjectRotation = Quaternion.LookRotation(hit.normal);
+            portal.transform.position = hit.point;
+            portal.transform.rotation = hitObjectRotation;
+        }
+
+    }
+
+}
