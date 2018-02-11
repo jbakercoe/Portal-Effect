e2d387bb6c1925b92a5264cb9d9c41f1586f9b23
Jbakercoe <jbakercoe@gmail.com>
2018-02-11 14:36:00 -0500

    Update Scene

---
+++ Portals/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Restart()
+    {
+        player.transform.position = spawnPoint.transform.position;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
