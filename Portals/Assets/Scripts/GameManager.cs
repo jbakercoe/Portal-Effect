d9305349ce386bdc38571a73effc3b034e46b697
Jbakercoe <jbakercoe@gmail.com>
2018-02-15 16:18:16 -0500

    Add Crosshair

--- Portals/Assets/Scripts/GameManager.cs
+++ Portals/Assets/Scripts/GameManager.cs
@@ -9,12 +9,7 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        Cursor.visible = false;
 	}
 
     public void Restart()
